20f4bbdcc869267ab2482b50e90a7aa0f801ac7c
NguyenTruongKhangThuan <ntkthuan1209@gmail.com>
2024-06-04 15:20:01 +0700

    Successfully Add Hooks into the Site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
+import UserHeader from "@/components/shared/user/user-header";
+import UserFooter from "@/components/shared/user/user-footer";
+
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <main>
+          <UserHeader />
+          {children}
+          <UserFooter />
+        </main>
+      </body>
     </html>
   );
 }
---
+++ src/components/shared/user/product-card.tsx
@@ -0,0 +1,59 @@
+import { Product } from '@/types/product'
+import React from 'react'
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+interface ProductCardProps {
+    product: Product,
+    //Type of Product
+    //Normal: Regular product
+    //Discount: Product on discount
+    //Special: Special Product (e.g. New Arrival, Limited Time Offer)
+    type: 'normal' | 'discount' | 'special',
+}
+
+const RtoS = (rating: number) => {
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+        stars.push(<i className='fas fa-star text-yellow-400'></i>);
+    }
+    return stars;
+
+}
+
+const ProductCard : React.FC<ProductCardProps> = ({
+    product,
+    type
+}) => {
+  return (
+    <Card id={product.id.toString()}>
+        <CardHeader>
+            <img src={product.imageUrl} alt={product.name} />
+        </CardHeader>
+        <CardContent>
+            <CardDescription>{product.name}</CardDescription>
+        </CardContent>
+        <CardFooter>
+            <div className='flex items-center gap-4'>
+                {/* Rating Points */}
+                <div className='flex gap-1'>
+                    {RtoS(product.productRating.rating)}
+                </div>
+                {/* Review Count */}
+                <p>{product.productRating.reviewCount}</p>
+            </div>
+            {/* Price */}
+            <p>${product.price}</p>
+        </CardFooter>
+    </Card>
+  )
+}
+
+export default ProductCard
