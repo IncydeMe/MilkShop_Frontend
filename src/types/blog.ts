bdc70c074bff741aeb4f5c284af1d75031063c10
NguyenTruongKhangThuan <ntkthuan1209@gmail.com>
2024-07-06 16:02:39 +0700

    Update blog.ts

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -32,14 +32,3 @@ export type Blog = {
     imageUrl: string;
 }
 
-/**
- * Blog Category, used to store the details of a blog category
- * @param blogCategoryId: number, the id of the blog category
- * @param name: string, the name of the blog category
- */
-export type BlogCategory = {
-    /** Blog Category Id */
-    blogCategoryId: number;
-    /** Name of the blog category */
-    name: string;
-}
